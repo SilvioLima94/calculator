c0bd146b6dc3f1f0f432252637d138e098c6315d
Silvio Pina <silviopina94@gmail.com>
2023-02-02 21:18:46 -0500

    added display values to show on the calculator display

--- index.js
+++ index.js
@@ -28,4 +28,21 @@ function operate(op, num1, num2) {
   }
 }
 
-let displayValue = null;
+let displayWrapper = document.querySelector(".display-wrapper");
+//console.log(displayValue);
+let display = document.createElement("div");
+
+let btns = document.querySelectorAll("button");
+btns.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    display.innerText = e.target.value;
+    return displayWrapper.appendChild(display);
+  });
+});
+
+let clear = document.querySelectorAll("button")[0];
+let clearDisplay = clear.addEventListener("click", () =>
+  clear.getAttribute("0")
+);
+
+console.log(clear);
