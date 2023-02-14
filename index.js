844908c4dbd29a34b44e902d0d58378f1c6bd417
Silvio Pina <silviopina94@gmail.com>
2023-02-14 18:30:22 -0500

    added all the functionalities to the operator buttons to work as expected

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-  return num1 * num2;
+  return num1 / num2;
 }
 
 function operate(operator, num1, num2) {
-  if (operator === "addition") {
+  if (operator === "addition" || operator === "add") {
     return add(num1, num2);
   } else if (operator === "subtract") {
     return subtract(num1, num2);
@@ -29,86 +29,68 @@ function operate(operator, num1, num2) {
 }
 
 function populate() {
-  let currentOperation = document.querySelector(".current-operation");
-  let display;
   let btns = document.querySelectorAll("button");
-  let fragment = document.createDocumentFragment();
+  let currentOperation = document.querySelector(".current-operation");
   let lastOperation = document.querySelector(".last-operation");
-  let result,
-    val1,
-    val2 = null;
+  let bottomScreen = document.querySelector("#bottom-screen");
+  let display, current, result, newNum, currentOperator;
+  let operatorArray = ["divide", "multiply", "subtract", "add"];
 
   btns.forEach((button) => {
     button.addEventListener("click", (e) => {
-      display = document.createElement("div");
-      display.className = "display";
-      display.innerText = e.target.value;
+      display = e.target.value;
 
-      if (display.innerText === "clear") {
-        let removeEl = lastOperation;
-        removeEl.removeChild(removeEl.lastElementChild);
-        // val1 = null;
-        // console.log(val1, "Array is now empty!");
-        return document
-          .querySelectorAll(".display")
-          .forEach((div) => div.parentNode.removeChild(div));
+      let operatorMatch = operatorArray.filter((op) => op === display).join("");
+      console.log("operatorMatch: " + operatorMatch);
+
+      let operatorSign =
+        operatorMatch === "divide"
+          ? " / "
+          : operatorMatch === "multiply"
+          ? " * "
+          : operatorMatch === "subtract"
+          ? " - "
+          : operatorMatch === "add"
+          ? " + "
+          : "";
+      console.log(operatorSign);
+
+      if (display === "clear") {
+        lastOperation.innerHTML = "";
+        return (currentOperation.innerHTML = "");
       } else if (
-        display.innerText !== "delete" &&
-        display.innerText !== "multiply" &&
-        display.innerText !== "divide" &&
-        display.innerText !== "subtract" &&
-        display.innerText !== "add" &&
-        display.innerText !== "equal"
+        display !== "delete" &&
+        display !== "multiply" &&
+        display !== "divide" &&
+        display !== "subtract" &&
+        display !== "add" &&
+        display !== "equal"
       ) {
-        fragment.appendChild(display);
-        return currentOperation.appendChild(fragment);
-      } else if (display.innerText === "delete") {
-        const removeElement = currentOperation;
-        return removeElement.removeChild(removeElement.lastElementChild);
+        return currentOperation.append(e.target.value);
+      } else if (display === "delete") {
+        // const removeElement = Array.from(currentOperation);
+        // console.log(removeElement);
+      } else if (display === operatorMatch) {
+        currentOperator = operatorMatch;
+        current = Number(currentOperation.outerText);
+        console.log(typeof current);
+        console.log("current: " + current);
+        lastOperation.append(current + operatorSign);
+        bottomScreen.innerHTML = "";
+      } else if (display === "equal") {
+        //console.log("equal...");
+        newNum = Number(currentOperation.outerText);
+        console.log("newNum");
+        console.log(newNum);
+        lastOperation.append(newNum);
+        console.log(operatorMatch);
+        result = operate(currentOperator, current, newNum);
+        console.log("result: " + result);
+        currentOperation.innerHTML = "";
+        currentOperation.append(result.toFixed(3));
       } else {
-        console.log(currentOperation);
-        let extractedNumbersArray = Array.from(currentOperation.outerText)
-          .filter((item) => item !== "\n")
-          .map((item) => item);
-        console.log(extractedNumbersArray);
-        val1 = parseFloat(extractedNumbersArray.map((num) => num).join(""));
-
-        let operator = display.innerText;
-        if (operator === "multiply") {
-          let lastResult = document.createElement("div");
-          lastResult.innerText = val1 + " x";
-          lastResult.className = "last-value";
-          fragment.appendChild(lastResult);
-          lastOperation.appendChild(fragment);
-
-          //   if (currentOperation.hasChildNodes()) {
-          //     //console.log(currentOperation);
-          //     const removeEl = display;
-          //     removeEl.remove();
-          //   }
-          //   if (e.target.value) {
-          //     let newDiv = document.createElement("div");
-          //     fragment.appendChild(newDiv);
-          //     currentOperation.appendChild(fragment);
-
-          //     //console.log(currentOperation);
-          //   }
-          if (currentOperation.hasChildNodes) {
-            currentOperation.removeChild(currentOperation.children[0]);
-          }
-          //val2 = e.target.value;
-          //console.log("val2 " + val2);
-        }
-
-        result = operate(operator, val1, 10);
-        console.log(result);
-
-        // if (display.innerText === "equal") {
-
-        // }
+        return;
       }
-
-      //console.log(val1);
     });
   });
 }
